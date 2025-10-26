1ab97b6afd1001d0c252d2a213b35a06fd40f181
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-26 18:46:43 -0300

    Update constant value
    Co-Authored-By: Tobias-Noceda <112185326+tobias-noceda@users.noreply.github.com>

--- src/main/java/org/sims/sand/SandForce.java
+++ src/main/java/org/sims/sand/SandForce.java
@@ -11,7 +11,7 @@ import org.sims.neighbours.*;
 record SandForce() implements Force<Particle, SandForce.Data> {
     public static final double Kn = 250;
     public static final double GAMMA = 1;
-    public static final double MU = 0.25;
+    public static final double MU = 0.7;
 
     @Override
     public Map<Particle, Vector2> apply(
