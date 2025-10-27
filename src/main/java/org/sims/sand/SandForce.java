203388efc2dc7e59754218d7af712cc91f6fe82a
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-27 04:50:50 -0300

    perf

--- src/main/java/org/sims/sand/SandForce.java
+++ src/main/java/org/sims/sand/SandForce.java
@@ -1,7 +1,6 @@
 package org.sims.sand;
 
 import java.util.*;
-import java.util.function.*;
 import java.util.stream.*;
 
 import org.sims.interfaces.*;
@@ -17,19 +16,19 @@ record SandForce() implements Force<Particle, SandForce.Data> {
     public Map<Particle, Vector2> apply(final Collection<Particle> particles, final Data data) {
         final var neighbours = data.cim().evaluate(particles);
 
-        return particles.parallelStream()
-                .collect(Collectors.toMap(Function.identity(), p -> {
-                    final var interactives = Stream
-                            .concat(
-                                    neighbours.get(p).stream(),
-                                    data.walls().stream().filter(w -> w.overlap(p) > 0))
-                            .toList();
+        final var forces = new HashMap<Particle, Vector2>(particles.size() + 1, 1.0f);
+        for (final var p : particles) {
+            final var interactives = Stream
+                    .concat(
+                            neighbours.get(p).stream(),
+                            data.walls().stream().filter(w -> w.overlap(p) > 0))
+                    .toList();
 
-                    final var gravity = Forces.gravity(p);
-                    final var overlap = Forces.overlap(p, interactives, Kn, GAMMA, MU);
+            forces.merge(p, Forces.gravity(p), Vector2::add);
+            forces.merge(p, Forces.overlap(p, interactives, Kn, GAMMA, MU), Vector2::add);
+        }
 
-                    return gravity.add(overlap);
-                }));
+        return forces;
     }
 
     public record Data(CIM cim, Collection<? extends Interactive<Particle>> walls) {
